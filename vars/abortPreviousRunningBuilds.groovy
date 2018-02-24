b0eb2706f3ab53b680e701b98cb6598690b5f0f2
Kyle Barton <kyle.humberto@gmail.com>
2018-02-23 17:16:20 -0800

    Only abort the previous build

--- vars/abortPreviousRunningBuilds.groovy
+++ vars/abortPreviousRunningBuilds.groovy
@@ -4,19 +4,17 @@ def call() {
     def hi = Hudson.instance
     def pname = env.JOB_NAME.split('/')[0]
 
-    hi.getItem(pname).getItem(env.JOB_BASE_NAME).getBuilds().each{ build ->
-        def exec = build.getExecutor()
+    // get the previous build
+    def build = hi.getItem(pname).getItem(env.JOB_BASE_NAME).getBuilds()[1]
+    def exec = build.getExecutor()
 
-        if (build.number != currentBuild.number && exec != null) {
-            exec.interrupt(
-                Result.ABORTED,
-                new CauseOfInterruption.UserInterruption(
-                    "Aborted by #${currentBuild.number}"
-                )
+    if (build.number != currentBuild.number && exec != null) {
+        exec.interrupt(
+            Result.ABORTED,
+            new CauseOfInterruption.UserInterruption(
+                "Aborted by #${currentBuild.number}"
             )
-            println("Aborted previous running build #${build.number}")
-        } else {
-            println("Build is not running or is current build, not aborting - #${build.number}")
-        }
+        )
+        println("Aborted previous running build #${build.number}")
     }
 }
